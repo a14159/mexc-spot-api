4e2142fd73cc36b9e69d8e9562e2b4206e7778c5
kong <kong_k@hotmail.com>
2025-06-13 18:06:46 +0300

    24hTicker impl

---
+++ src/main/java/io/contek/invoker/mexc/spot/api/common/_24hTicker.java
@@ -0,0 +1,31 @@
+package io.contek.invoker.mexc.spot.api.common;
+
+import javax.annotation.concurrent.NotThreadSafe;
+import java.math.BigDecimal;
+
+@NotThreadSafe
+public class _24hTicker {
+  //     "symbol": "BTCUSDT",
+  //    "priceChange": "184.34",
+  //    "priceChangePercent": "0.00400048",
+  //    "prevClosePrice": "46079.37",
+  //    "lastPrice": "46263.71",
+  //    "bidPrice": "46260.38",
+  //    "bidQty": "",
+  //    "askPrice": "46260.41",
+  //    "askQty": "",
+  //    "openPrice": "46079.37",
+  //    "highPrice": "47550.01",
+  //    "lowPrice": "45555.5",
+  //    "volume": "1732.461487",
+  //    "quoteVolume": null,
+  //    "openTime": 1641349500000,
+  //    "closeTime": 1641349582808,
+  //    "count": null
+
+  public String symbol;
+
+  public BigDecimal lastPrice;
+  public BigDecimal volume;
+  public BigDecimal quoteVolume;
+}
