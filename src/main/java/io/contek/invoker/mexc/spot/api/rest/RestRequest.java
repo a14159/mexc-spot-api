806d82abde4dd9fac76a60d96fb68f0ff5ddf926
kong <kong_k@hotmail.com>
2025-06-27 00:51:46 +0300

    finally fixing all signing issues?

--- src/main/java/io/contek/invoker/mexc/spot/api/rest/RestRequest.java
+++ src/main/java/io/contek/invoker/mexc/spot/api/rest/RestRequest.java
@@ -35,64 +35,44 @@ public abstract class RestRequest<R> extends BaseRestRequest<R> {
   @Override
   protected final RestCall createCall(ICredential credential) {
     RestMethod method = getMethod();
+    String paramsString = buildParamsString();
+    if (!credential.isAnonymous()) {
+      StringBuilder sb = new StringBuilder(128);
+      sb.append(paramsString);
+      String toSign = paramsString;
+      // if no timestamp param append it
+      if (paramsString.lastIndexOf("timestamp") < 0) {
+        String timestamp = Long.toString(clock.millis());
+
+        if (paramsString.isEmpty()) {
+          sb.append("?");
+        } else {
+          sb.append("&");
+        }
+        sb.append("timestamp=");
+        sb.append(timestamp);
+        toSign = sb.toString();
+      }
+      String signature = credential.sign(toSign.substring(1)); // remove "?" at the beginning
+      sb.append("&signature=");
+      sb.append(signature);
+      paramsString = sb.toString();
+    }
     switch (method) {
       case GET, DELETE -> {
-          String paramsString = buildParamsString();
-          if (!credential.isAnonymous()) {
-            StringBuilder sb = new StringBuilder(128);
-            sb.append(paramsString);
-            String toSign = paramsString;
-            // if no timestamp param append it
-            if (paramsString.lastIndexOf("timestamp") < 0) {
-              String timestamp = Long.toString(clock.millis());
-
-              if (paramsString.isEmpty()) {
-                sb.append("?");
-              } else {
-                sb.append("&");
-              }
-              sb.append("timestamp=");
-              sb.append(timestamp);
-              toSign = sb.toString();
-            }
-            String signature = credential.sign(toSign.substring(1)); // remove "?" at the beginning
-            sb.append("&signature=");
-            sb.append(signature);
-            paramsString = sb.toString();
-          }
           return RestCall.newBuilder()
             .setUrl(buildUrlString(paramsString))
             .setMethod(method)
-            .setHeaders(generateHeaders(credential))
+            .setHeaders(generateHeaders(credential, method))
             .build();
       }
       case POST, PUT -> {
-          RestMediaBody body = JSON.createBody(getParams());
-          String params = "";
-          if (!credential.isAnonymous()) {
-            String timestamp = Long.toString(clock.millis());
-            StringBuilder payloadSB = new StringBuilder(128);
-            String signedBody = body.getStringValue();
-            if (signedBody.equals("{}"))
-              signedBody = "";
-            String payload = payloadSB
-                    .append("timestamp=")
-                    .append(timestamp)
-                    .append(signedBody)
-                    .toString();
-            String signature = credential.sign(payload);
-            params = new StringBuilder(64)
-                    .append("?timestamp=")
-                    .append(timestamp)
-                    .append("&signature=")
-                    .append(signature).toString();
-          }
-          return RestCall.newBuilder()
-            .setUrl(buildUrlString(params))
-            .setMethod(method)
-            .setHeaders(generateHeaders(credential))
-            .setBody(body)
-            .build();
+        return RestCall.newBuilder()
+                .setUrl(buildUrlString(paramsString))
+                .setMethod(method)
+                .setHeaders(generateHeaders(credential, method))
+                .setBody(JSON.createBody(RestParams.empty()))
+                .build();
       }
       default -> throw new IllegalStateException(getMethod().name());
     }
@@ -101,14 +81,18 @@ public abstract class RestRequest<R> extends BaseRestRequest<R> {
   @Override
   protected final void checkResult(R result, RestResponse response) throws AnyHttpException {}
 
-  private Map<String, String> generateHeaders(ICredential credential) {
+  private Map<String, String> generateHeaders(ICredential credential, RestMethod method) {
     if (credential.isAnonymous()) {
       return Map.of();
     }
 
-    return Map.of(
-            "X-MEXC-APIKEY", credential.getApiKeyId(),
-            "Content-Type", "application/json");
+    return switch (method) {
+      case GET, DELETE -> Map.of(
+              "X-MEXC-APIKEY", credential.getApiKeyId());
+      case POST, PUT ->     Map.of(
+              "X-MEXC-APIKEY", credential.getApiKeyId(),
+              "Content-Type", "application/json");
+    };
   }
 
   private String buildParamsString() {
