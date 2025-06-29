51e14bf24a4b5a09c5165f0076a5904aa13c60a8
kong <kong_k@hotmail.com>
2025-06-29 16:47:38 +0300

    _Order changes according to MexC magic API

--- src/main/java/io/contek/invoker/mexc/spot/api/common/_Order.java
+++ src/main/java/io/contek/invoker/mexc/spot/api/common/_Order.java
@@ -7,18 +7,13 @@ import java.math.BigDecimal;
 public class _Order {
 
   public String symbol;
-  public String id;
+  public String orderId;
   public String clientOrderId;
   public BigDecimal price;
-  public BigDecimal quantity;
-  public BigDecimal amount;
-  public BigDecimal avgPrice;
-  public int orderType; // Order type: LIMIT_ORDER (1), POST_ONLY (2), IMMEDIATE_OR_CANCEL (3), FILL_OR_KILL (4), MARKET_ORDER (5); Stop loss/take profit (100)
-  public int tradeType; // Trade type (1: Buy, 2: Sell)
-  public BigDecimal remainAmount;
-  public BigDecimal remainQuantity;
-  public BigDecimal cumulativeQuantity;
-  public BigDecimal cumulativeAmount;
-  public int status; // Order status: 1: Not traded, 2: Fully traded, 3: Partially traded, 4: Canceled, 5: Partially canceled
-  public long createTime;
+  public BigDecimal origQty;
+  public BigDecimal executedQty;
+  public String type; // Order type: LIMIT_ORDER (1), POST_ONLY (2), IMMEDIATE_OR_CANCEL (3), FILL_OR_KILL (4), MARKET_ORDER (5); Stop loss/take profit (100)
+  public String side; // Trade type (1: Buy, 2: Sell)
+  public String status; // Order status: 1: Not traded, 2: Fully traded, 3: Partially traded, 4: Canceled, 5: Partially canceled
+  public long time;
 }
